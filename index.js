32452f911c6529d9d5ba07b0005901e17e84aaa3
yuiseki <yuiseki@gmail.com>
2020-01-25 20:11:07 +0900

    update for setting window html

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 
 // Electronのモジュール
 const electron = require("electron");
+const preference = require('electron-preference');
+
+const path = require('path');
+
 const Tray = electron.Tray;
 const Menu = electron.Menu;
 
@@ -14,13 +18,41 @@ let settingsWindow;
 // trayもGCされないようにグローバル宣言
 let tray = null;
 
+
+
 // 起動時に実行される処理
 app.on('ready', function() {
   createTray();
+  setInterval(intervalFunction, 60000);
 });
 
+function intervalFunction(){
+  uploadFullScreenCapture();
+  uploadDownloadDir();
+}
 
-// 通知領域を実装する関数
+// 全画面をキャプチャしてGyazoにアップロードする関数
+function uploadFullScreenCapture(){
+
+}
+
+// ダウンロードフォルダ内の画像をすべてGyazoにアップロードする関数
+function uploadDownloadDir(){
+
+}
+
+function showSettingsWindow(){
+  let settingsWindow = new BrowserWindow({ width: 800, height: 600 })
+  settingsWindow.on('closed', () => {
+    settingsWindow = null
+  })
+  // または、ローカルファイルをロード
+  settingsWindow.loadURL(`file://${__dirname}/public/index.html`)
+}
+
+
+
+// 通知領域にアイコンを表示する関数
 function createTray(){
   // 通知領域に表示するアイコンを指定（必須）
   tray = new Tray('./public/images/ninja.png');
@@ -30,7 +62,7 @@ function createTray(){
     {
       label: '設定',
       click (menuItem){
-        settingsWindow()
+        showSettingsWindow();
       }
     },
     {
